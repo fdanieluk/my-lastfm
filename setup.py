2a09b6a102ed001d9e19463ddb48360dcb73dc3f
Filip Danieluk <filip.danieluk94@gmail.com>
2019-03-23 18:14:06 +0100

    make pip installable & minor changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='mylastfm',
+      version='0.0.1',
+      description='LastFm visualization project',
+      url='https://github.com/imintogoodmusic/my-lastfm',
+      author='Filip Danieluk',
+      author_email='filip.danieluk94@gmail.com',
+      license='MIT',
+      packages=['lastfm.src', 'lastfm'],
+zip_safe=False)
