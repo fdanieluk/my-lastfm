40a69d62a255c5683ad7306fd2c952747fc83c46
Filip Danieluk <filip.danieluk94@gmail.com>
2019-03-23 18:13:28 +0100

    connect to lastfm api

---
+++ lastfm/src/authentication.py
@@ -0,0 +1,15 @@
+from pylast import LastFMNetwork, md5
+import json
+
+from lastfm import ROOT
+
+
+def authenticate():
+    with open(ROOT + 'credentials.json') as f:
+        credentials = json.load(f)
+
+    network = LastFMNetwork(api_key=credentials['API_key'],
+                            api_secret=credentials['shared_secret'],
+                            username=credentials['username'],
+                            password_hash=md5(credentials['password']))
+    return network
